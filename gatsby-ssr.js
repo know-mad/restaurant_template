61252a0b9c12676bfd422446a5e0084ab2ec4c98
David_Velez <velezdavid6@gmail.com>
2021-09-18 15:29:00 -0400

    fixed SSR bug

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -8,15 +8,15 @@
 
 import React from "react"
 import { CartProvider } from "use-shopping-cart"
-
+const isBrowser = typeof window !== "undefined"
 export const wrapRootElement = ({ element }) => (
     // Create a customer provider to get customer details (phone number) into metadata field
         <CartProvider
         mode='payment'
         cartMode="client-only"
         stripe={process.env.GATSBY_STRIPE_PUBLISHABLE_KEY}
-        successUrl={`${window.location.origin}/order-online/success/`}
-        cancelUrl={`${window.location.origin}/checkout/`}
+        successUrl={isBrowser && `${window.location.origin}/order-online/success/`}
+        cancelUrl={isBrowser && `${window.location.origin}/checkout/`}
         currency="USD"
         allowedCountries={['US', 'GB', 'CA']}
         billingAddressCollection={true}
