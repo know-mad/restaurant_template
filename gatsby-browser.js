4490340b32d7bcd410f01cabb9dfdc957d704948
David_Velez <velezdavid6@gmail.com>
2021-09-19 12:20:48 -0400

    updated SSR

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,11 +1,3 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
-
 import React from "react"
 import { CartProvider } from "use-shopping-cart"
 const isBrowser = typeof window !== "undefined"
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,11 +1,3 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/ssr-apis/
- */
-
-// You can delete this file if you're not using it
-
 import React from "react"
 import { CartProvider } from "use-shopping-cart"
 const isBrowser = typeof window !== "undefined"
